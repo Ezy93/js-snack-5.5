68fdcaf8c4f0f21d50f5107810807b214b39295b
Ezy93 <giordani001@gmail.com>
2022-03-14 11:56:17 +0100

    restyle

--- js-snack -B/js/main.js
+++ js-snack -B/js/main.js
@@ -1,5 +1,5 @@
 
-    /* A - Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */
+/* A - Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */
     function stringReverse (stringa){
         
             let splString = stringa.split("");
@@ -13,7 +13,7 @@
 
     console.log(stringReverse("giovanni"));
 
-    /* B - Scrivi una funzione che accetti un numero come argomento e ritorni il suo opposto (inverso additivo) es (34 -> -34) */
+/* B - Scrivi una funzione che accetti un numero come argomento e ritorni il suo opposto (inverso additivo) es (34 -> -34) */
 
     function oppositeNumber (number){
         return number - (number*2)
@@ -21,7 +21,8 @@
 
     console.log(oppositeNumber(-10));
 
-    /* C - Data una lista di 10 numeri e 10 stringhe, restituisci una copia della lista con tutte le stringhe girate e i numeri opposti. */
+    
+/* C - Data una lista di 10 numeri e 10 stringhe, restituisci una copia della lista con tutte le stringhe girate e i numeri opposti. */
 
 
 const stringhe = ["giovanni","riccardo","teseo","romolo","remolo","teodoro","franco","mafaldo","erwin","asterix"];
