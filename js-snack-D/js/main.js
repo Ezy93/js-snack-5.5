920f00f73de6e03b411010a02a285dc754dfc3ee
Ezy93 <giordani001@gmail.com>
2022-03-14 12:46:18 +0100

    completato js snack D

--- js-snack-D/js/main.js
+++ js-snack-D/js/main.js
@@ -6,3 +6,22 @@
     * Scrivi una funzione che accetti tre argomenti: l'array e i due numeri nell'ordine scritto prima.
     * La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra il primo numero e il secondo numero
 */
+
+const arrayMix = [1, 2, 3, "giovanni", "laura", "esteban", 4];
+const newArray = [];
+
+let randomIntA = Math.floor(Math.random()* arrayMix.length +1);
+console.log(randomIntA);
+
+let randomIntB = Math.floor(Math.random()* randomIntA  );
+console.log(randomIntB);
+
+
+function indexSelector( array , numberA , numberB){
+    for(let i = numberB; i < numberA ; i++){
+        newArray.push(array[i]);
+    }
+    return newArray;
+};
+
+console.log(indexSelector(arrayMix, randomIntA, randomIntB));
